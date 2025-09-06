61a12f19ea62bdaaa5e4ea798c2791a745ad456d
Alex Berg <apb@njit.edu>
2025-09-06 19:32:44 -0400

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,24 @@
+import requests
+
+api_url = "https://student-info-api.netlify.app/.netlify/functions/submit_student_info"
+
+data = {
+    "UCID": input("Enter your UCID: "),
+    "first_name": input("Enter your first name: "),
+    "last_name": input("Enter your last name: "),
+    "github_username": input("Enter your github username: "),
+    "discord_username": input("Enter your discord username: "),
+    "favorite_cartoon": input("Enter your favorite cartoon: "),
+    "favorite_language": input("Enter you favorite programming language: "),
+    "movie_or_game_or_book": input("Enter favorite movie, game, or book: "),
+    "section": input("Enter your section number: ")
+}
+
+params = {"UCID": "apb", "section": "101"}
+response = requests.get(api_url, params=params)
+if(response.status_code == 200 and response.json()):
+    print("Cannot add duplicate results")
+if(response.status_code != 200):
+    print("Request information may contain errors")
+else:
+    response = requests.post(api_url, json=data)
